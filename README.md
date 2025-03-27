b5b86de060cdd26e49701fecde33f792021987c6
ChickenLitte <145792244+ChickenLitte@users.noreply.github.com>
2025-03-27 16:42:22 +0000

    v4 Done. But no extra features

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# gilmit
+
+---
+# VersionControl.py - User Guide
+
+**`VersionControl.py` is a program designed to help the user easily keep track of the changes across multiple files!**
+
+![what running the program might look like!](Example.png)
+
+1. **Setup**: Ensure you have Python installed on your system. Clone the repository containing `VersionControl.py` to your local machine.
+
+2. **Features**:
+    - **Save Files**: Use the program to save personal files by their identifying name and insert the file right into the sistem from the terminal!
+    - **Save Versions**: With this program, you not only have the ability to save files, but also to track the version history of these files as each version and it's changes are saved in the system when you save a file twice!
+    - **Restore a File**: Since the version history of a file is saved, you can go back to and restore any previous version of a file you saved to the system!
+    - **View a log**: Review the history of changes and commits accross all files, at the same time!.
+
+3. **Running the Program**: Open a terminal, navigate to the directory containing `VersionControl.py`, and execute the script using:
+    ```bash
+    python VersionControl.py
+    ```
+
+4. **Using the program**: The program is designed in regards to having the best user experience, and easy user interaction! Being said, through the terminal, the program will guide you through each step of the process and always remind you of the possible commands!
+
+5. **Exiting**: At any time when you want to quit or exit the program all you have to do is type the word "exit"!
+
+ 
+## CheetSheet
+
+- **"save file"**: Guides the user through the process of saving previous or new file/version!
+- **"restore version:"** Guides the user through the process of restoring a saved file version!
+- **"print log:"** Dysplays a "map" of all the files, their versions, and each version's corresponding changes!
+- **"exit"** Exits the program!
+
+###### Refer to the inline documentation in `VersionControl.py` for more details on specific commands mechanics.
--- VersionControl.py
+++ VersionControl.py
@@ -11,27 +11,34 @@
 from friendsbalt.acs import AVLTree, StringDiff, OrderedMap#, HuffmanFile
 from time import sleep
 import random
+from colorama import Fore, Back, Style, init
 
 class VersionControl:
     
     def __init__(self):#initialize empty file and version history tree
-        self.files = OrderedMap()
-        self.old_input = input
 
-    def saveCurrent(self, filename, file):
-        if self.files.__contains__(filename):
+        self.files = OrderedMap()
+        self.old_input = input#store the old input function
+        init(autoreset=True)#for the colors
+
+    def saveCurrent(self, filename, file):#save the current version of the file
+
+        if self.files.__contains__(filename):#if the file already exists, add the new version
+
             file_entry = self.files.__getitem__(filename)
             self.version = file_entry.__len__()
-            file_entry.__setitem__(self.version, StringDiff(file_entry.__getitem__(self.version-1), file))
-        else:
-            # Ensure the value for the filename key is an OrderedMap
+            file_entry.__setitem__(self.version, StringDiff(file_entry.__getitem__(self.version-1), file))#add the new version
+
+        else:#if the file version doesn't already exist, create it
+
+            # Make sure that the value for the filename key is an OrderedMap
             self.files.__setitem__(filename, OrderedMap())
             self.version = 0
-            self.files.__getitem__(filename).__setitem__(self.version,file)
+            self.files.__getitem__(filename).__setitem__(self.version,file)#add the new version
 
-        self.input("saved file: " + filename + ", version: " + str(self.version) + " \n", True)
+        self.input("saved file: " + filename + ", version: " + str(self.version) + " \n", True)#print a confirmation
 
-    def restoreVersion(self, filename, version):#returns the restored version, recursive
+    def restoreVersion(self, filename, version):#returns the restored version, iterative; could be recursive
         
         restoredFile = self.files.__getitem__(filename).__getitem__(0)
         for key in list(self.files.__getitem__(filename).tree.keys())[1:]:
@@ -46,56 +53,83 @@ class VersionControl:
 
         self.input("returning log:\n",True)
         
-        for key in self.files.tree.keys():
+        for key in self.files.tree.keys():#print the file names
+
             copy = key
             self.input("- file: " + str(key) + "\n",True)
-            for key2 in self.files.__getitem__(copy).tree.keys():
+
+            for key2 in self.files.__getitem__(copy).tree.keys():#print the version history of the file
+
                 self.input("   - version: " + str(key2),True)
                 self.input("diff: " + str(self.files.__getitem__(copy).__getitem__(key2)) + "\n",True)
             
-    def input(self, str, inPUT = False):
+    def input(self, str, inPUT = False):#acts as both a print and input function, with a dramatic slow type effect
+            
             i = 0
             # print(len(str))
-            while i < (len(str) - 1):
-                print(str[i], sep='', end='', flush=True)
-                sleep(0.01)
+
+            while i < (len(str) - 1):#slowly print the string
+
+                print(Fore.BLACK + Back.WHITE + Style.BRIGHT,str[i], sep='', end='', flush=True)#no endline,hopefully with colors
+                sleep(0.025)
                 i += 1
             
-            if inPUT:
-                print(str[i], sep='', end='', flush=True)
+            if inPUT:#use the old input function for the last character
+                print(Fore.BLACK + Back.WHITE + Style.BRIGHT, str[i], sep='', end='', flush=True)#just a normal printfunction
                 return 
-            return self.old_input(str[i])
+            
+            return self.old_input(str[i])#return the input from the user
 
 def main():
-    vc = VersionControl()
+
+    vc = VersionControl()#initialize the vc system
     
-    name = vc.input(str = "Who should I store these files after?\n > ")
-    command = vc.input("\nHello " + name + ", here are the commands:\n - save file \n - restore version \n - print log \n - exit \n What would you like to do? \n > ")
-    commandList = {"save file":vc.saveCurrent,"restore version":vc.restoreVersion, "print log":vc.returnLog, "exit":exit}
+    name = vc.input(str = "Who should I store these files after?\n > ")#get the user's name
+    command = vc.input("\nHello " + name + ", here are the commands:\n - save file \n - restore version \n - print log \n - exit \n\n What would you like to do? \n > ")#ask for the initial command
+    commandList = {"save file":vc.saveCurrent,"restore version":vc.restoreVersion, "print log":vc.returnLog, "exit":exit}# a dictionary linking user commands and actions for the program
     
-    insultList = [
+    insultList = [#for laughs, random insults
         "Who taught " + name + " how to type!",
         "Oh brother, get this " + name + " guy outa here! \n",
-        "Erm, " + name + " what the \u03A3"
+        "Erm, " + name + " what the \u03A3",
+        "You're not very good at this, are you " + name + "?\n",
+        "Boy, " + name + ", someone needs to go back to typing school!\n",
+        "Congrats, " + name +  " you're the worst typer I've ever seen!\n",
+        name + " how would your mother feel about your typing skills?\n",
+        name + " Are your fingers fat?\n",
+        "You know " + name + ", they say, on the internet, no one knows you're a dog, but now I'm inclined disagree\n"
         ]
     
-    while True:
-        if command == "exit":
+    while True:#run the UI until the user exits
+        
+        if command == "exit":#builtin quit function
+
             vc.input("\nGoodbye " + name + "!\n", True)
             exit()
-        while command not in commandList:
+
+        while command not in commandList:#if the user writes a type, insult them and give them a list of options
+
             command = vc.input(random.choice(insultList) + "\nHere are the commands:\n - save file \n - restore version \n - print log \n - exit \n What would you like to do? \n > ")
-        if (command != "print log") and (command != "exit") and (command != "restore version"):
-            filename = vc.input("What's the file name?\n > ")
-            file = vc.input("What's the file?\n > ")
+
+        if (command != "print log") and (command != "exit") and (command != "restore version"):#for the "normal" commands
+
+            filename = vc.input("\nWhat's the file name?\n > ")
+            file = vc.input("\nWhat's the file?\n > ")
             commandList[command](filename,file)
-        elif command == "restore version":
-            filename = vc.input("What's the file name?\n > ")
-            version = vc.input("What's the version?\n > ")
+
+        elif command == "restore version":#needs a special case for the restore version command
+
+            filename = vc.input("\nWhat's the file name?\n > ")
+            version = vc.input("\nWhat's the version?\n > ")
+
             vc.input("\nRestored version: " + version + " of file: " + filename + ":\n", True)
             vc.input("      "+ commandList[command](filename,version) + "/n", True)
+
         else:
+
             commandList[command]()
+
+        #repeat the process
         command = vc.input("\nWhat would you like to do next?\n" + " here are the commands:\n - save file \n - restore version \n - print log \n - exit \n What would you like to do? \n > ")
 
 if __name__ == "__main__":
